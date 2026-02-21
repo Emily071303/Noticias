6443030740cea246475aa47e03338a0a43023ef2
Emily071303 <emilymope13@gmail.com>
2026-02-20 20:19:15 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.static('public'));
 
 const NEWS_TOKEN = process.env.NEWS_TOKEN;
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Endpoint de noticias principales
 app.get('/api/noticias/top', async (req, res) => {
